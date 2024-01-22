05effb19cae65cac677cf6dc8c1c8695d03d70ef
ahmed elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-01-22 19:10:56 +0200

    Adding Models and configuring  DbContext model

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace eTickets
 {
     public class Program
@@ -8,6 +11,9 @@ namespace eTickets
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            //DbContext Configuration
+            builder.Services.AddDbContext<AppDbContext>(options=>options
+            .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnictionString")));
 
             var app = builder.Build();
 
