402eb98b0bed2c34dce8f5096ac7a59fa8e17f2b
ahmed elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-01-23 22:00:40 +0200

    After creating index view for all models and editing the layout navbar

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -7,14 +7,14 @@ namespace eTickets.Controllers
     public class MoviesController : Controller
     {
         private readonly AppDbContext _context;
-        public MoviesController( AppDbContext context)
+        public MoviesController(AppDbContext context)
         {
             _context = context;
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
-            return View();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
+            return View(allMovies);
         }
     }
 }
