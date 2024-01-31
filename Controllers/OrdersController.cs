ca482ca5d0ff54954cf8267ee0e5d5c36fb3a827
ahmed elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-01-31 14:49:11 +0200

    After orders controller and views

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,67 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+        private readonly IOrdersService _ordersService;
+        public OrdersController(IMoviesService service, ShoppingCart shoppingCart, IOrdersService ordersService)
+        {
+            _moviesService = service;
+            _shoppingCart = shoppingCart;
+            _ordersService = ordersService;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+            var orders = await _ordersService.GetordersByUserIdAsync(userId);
+            return View(orders);
+        }
+        public IActionResult ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(response);
+        }
+
+        public async Task<IActionResult> AddToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+        public async Task<IActionResult> RmoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+        public async Task<IActionResult> CompleteOrder()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            string userId = "";
+            string userEmailAddress = "";
+
+            await _ordersService.StoreOrderAsync(items, userId, userEmailAddress);
+            await _shoppingCart.ClearShoppingCartAsync();
+            return View("CompleteOrder");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Cart;
 using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,6 +25,12 @@ namespace eTickets
             builder.Services.AddScoped<ICinemasService, CinemasService>();
             //Movies Services
             builder.Services.AddScoped<IMoviesService, MoviesService>();
+            //Orders Services
+            builder.Services.AddScoped<IOrdersService, OrdersService>();
+            //Configration for shopping cart
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+            builder.Services.AddSession();
             var app = builder.Build();
 
 
@@ -39,7 +46,7 @@ namespace eTickets
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseSession();
             app.UseAuthorization();
 
             app.MapControllerRoute(
